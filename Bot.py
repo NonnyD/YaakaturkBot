321f91398252b102a339162a82a981fd26d81343
NonnyD <sutinun230@gmail.com>
2025-06-21 21:52:32 +0700

    Update Bot.py

--- Bot.py
+++ Bot.py
@@ -5,6 +5,7 @@ from discord.ext import commands
 from keep_alive import keep_alive
 
 intents = discord.Intents.default()
+intents.message_content = True
 intents.members = True
 intents.voice_states = True
 
